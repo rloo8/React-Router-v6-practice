9f17dbca0ebef1aa7a4b459e2b7cbb6240fd52b8
rloo8 <141135378+rloo8@users.noreply.github.com>
2023-09-20 23:05:00 +0900

    #1 Header, Home, About

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Header";
+import { Outlet } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <hr />
+      <Outlet />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <div>
+      <h1>Best Seller Authors</h1>
+      <ul>
+        <li>J.K.Rowling</li>
+        <li>J.R.R.Tolkien</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Home;
